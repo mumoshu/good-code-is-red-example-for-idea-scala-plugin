8f83f39eac72a65bf0a223b600843618cc967b04
KUOKA Yusuke <ykuoka@gmail.com>
2011-03-13 13:26:07 +0900

    updated comments and README

--- redefining_type/src/application/controllers.scala
+++ redefining_type/src/application/controllers.scala
@@ -5,7 +5,9 @@ import framework.controller._
 //This also does not resolve DefaultController.
 //import framework.controller.DefaultController
 
+// 'DefaultController' is red.
 object Foos extends DefaultController {
+  // 'bar' is red.
   def test = bar
 }
 
